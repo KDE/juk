0ef66bceb2153af64de3167bef7a024044381e1c
Scott Wheeler <wheeler@kde.org>
2003-02-20 12:56:21 +0000

    Style guide fixes from Stefan Binner
    svn path=/branches/JUK_1_BRANCH/kdemultimedia/juk/; revision=208112

--- playlistbox.cpp
+++ playlistbox.cpp
@@ -129,7 +129,7 @@ void PlaylistBox::rename(PlaylistBoxItem *item)
 
     bool ok;
 
-    QString name = QInputDialog::getText(i18n("Rename..."), i18n("Please enter a name for this playlist:"),
+    QString name = QInputDialog::getText(i18n("Rename"), i18n("Please enter a name for this playlist:"),
 					 QLineEdit::Normal, item->text(), &ok);
     if(ok) {
 	item->setText(name);
@@ -158,7 +158,7 @@ void PlaylistBox::duplicate(PlaylistBoxItem *item)
 
 	// If this text is changed, please also change it in PlaylistSplitter::createPlaylist().
 
-	QString name = QInputDialog::getText(i18n("New Playlist..."), i18n("Please enter a name for the new playlist:"),
+	QString name = QInputDialog::getText(i18n("New Playlist"), i18n("Please enter a name for the new playlist:"),
 					     QLineEdit::Normal, splitter->uniquePlaylistName(item->text(), true), &ok);
 	if(ok) {
 	    Playlist *p = splitter->createPlaylist(name);
