2e35b01560119826a7bbf6116e91cdcc71d9c5d8
Scott Wheeler <wheeler@kde.org>
2003-09-09 02:09:39 +0000

    Make the hash class into a template class and provide specializations for QString and void *.
    svn path=/trunk/kdemultimedia/juk/; revision=249588

--- stringhash.cpp
+++ stringhash.cpp
@@ -17,115 +17,11 @@
 
 #include "stringhash.h"
 
-static const int tableSize = 5003;
-
-class StringHash::Node
-{
-public:
-    Node(const QString &value) : key(value), next(0) {}
-    ~Node() {}
-    
-    QString key;
-    Node *next;
-};
-
-StringHash::StringHash() : m_table(tableSize)
-{
-
-}
-
-StringHash::~StringHash()
-{
-    for(int i = 0; i < tableSize; i++)
-	deleteNode(m_table[i]);
-}
-
-bool StringHash::insert(const QString &value)
-{
-    int h = hash(value);
-    Node *i = m_table[h];
-    Node *j = 0;
-    
-    while(i) {
-	if(i->key == value)
-	    return true;
-	else {
-	    j = i;
-	    i = i->next;
-	}
-    }
-
-    if(j)
-	j->next = new Node(value);
-    else
-	m_table.insert(h, new Node(value));
-
-    return false;
-}
-
-bool StringHash::contains(const QString &value) const
-{
-    int h = hash(value);
-    Node *i = m_table[h];
-
-    while(i && i->key != value)
-	i = i->next;
-
-    return bool(i);
-}
-
-bool StringHash::remove(const QString &value)
-{
-    int h = hash(value);
-    Node *previous = 0;
-    Node *i = m_table[h];
-
-    while(i && i->key != value) {
-	previous = i;
-	i = i->next;
-    }
-
-    if(!i)
-	return false;
-
-    if(previous)
-	previous->next = i->next;
-    else {
-	if(i->next)
-	    m_table.insert(h, i->next);
-	else
-	    m_table.remove(h);
-    }
-
-    delete i;
-
-    return true;
-}
-
-/*
-QStringList StringHash::values() const
-{
-    QStringList l;
-
-    Node *n;
-
-    for(int i = 0; i < tableSize; i++) {
-	n = m_table[i];
-	while(n) {
-	    l.append(n->key);
-	    n = n->next;
-	}
-    }
-
-    return l;
-}
-*/
-
 ////////////////////////////////////////////////////////////////////////////////
 // private methods
 ////////////////////////////////////////////////////////////////////////////////
 
-int StringHash::hash(const QString &key) const
+int StringHash::hash(QString key) const
 {
     uint h = 0;
     uint g;
@@ -133,24 +29,16 @@ int StringHash::hash(const QString &key) const
     const QChar *p = key.unicode();
 
     for(int i = 0; i < int(key.length()); i++) {
-	h = (h << 4) + p[i].cell();
-	if((g = h & 0xf0000000))
-	    h ^= g >> 24;
-	h &= ~g;
+        h = (h << 4) + p[i].cell();
+        if((g = h & 0xf0000000))
+            h ^= g >> 24;
+        h &= ~g;
     }
 
     int index = h;
 
     if(index < 0)
-	index = -index;
+        index = -index;
 
-    return index % tableSize;
-}
-
-void StringHash::deleteNode(Node *n)
-{
-    if(n) {
-	deleteNode(n->next);
-	delete n;
-    }
+    return index % maxSize();
 }
--- stringhash.h
+++ stringhash.h
@@ -22,29 +22,137 @@
 #include <qstringlist.h>
 #include <qptrvector.h>
 
-class StringHash 
+template <class T> class Hash
 {
-public: 
-    StringHash();
-    ~StringHash();
+public:
 
-    bool insert(const QString &value);
-    bool contains(const QString &value) const;
-    bool remove(const QString &value);
+    bool insert(T value);
+    bool contains(T value) const;
+    bool remove(T value);
+
+protected:
+    Hash() : m_table(m_tableSize) {}
+    virtual ~Hash();
 
     /**
-     * Returns a sorted list of the values.
-     * Warning, this method is expensive and shouldn't be used except when 
-     * necessary.
+     * This should probably be done with partial template specialization, but
+     * I'm too lazy at the moment.
      */
-    // QStringList values() const;
-
+    virtual int hash(T key) const = 0;
+    static int maxSize() { return m_tableSize; }
 private:
-    class Node;
-    int hash(const QString &key) const;
+
+    struct Node
+    {
+        Node(T value) : key(value), next(0) {}
+        ~Node() {}
+        T key;
+        Node *next;
+    };
+
     void deleteNode(Node *n);
     
     QPtrVector<Node> m_table;
+    static const int m_tableSize = 5003;
 };
+
+class StringHash : public Hash<QString>
+{
+public:
+    StringHash() : Hash<QString>() {}
+    virtual int hash(QString) const;
+};
+
+class PtrHash : public Hash<void *>
+{
+public:
+    PtrHash() : Hash<void *>() {}
+    virtual int hash(void *key) const { return long(key) % maxSize(); }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// template method implementations
+////////////////////////////////////////////////////////////////////////////////
+
+template <class T>
+Hash<T>::~Hash()
+{
+    for(int i = 0; i < m_tableSize; i++)
+        deleteNode(m_table[i]);
+}
+
+template <class T>
+bool Hash<T>::insert(T value)
+{
+    int h = hash(value);
+    Node *i = m_table[h];
+    Node *j = 0;
+    
+    while(i) {
+        if(i->key == value)
+            return true;
+        else {
+            j = i;
+            i = i->next;
+        }
+    }
+
+    if(j)
+        j->next = new Node(value);
+    else
+        m_table.insert(h, new Node(value));
+
+    return false;
+}
+
+template <class T>
+bool Hash<T>::contains(T value) const
+{
+    int h = hash(value);
+    Node *i = m_table[h];
+
+    while(i && i->key != value)
+        i = i->next;
+
+    return bool(i);
+}
+
+template <class T>
+bool Hash<T>::remove(T value)
+{
+    int h = hash(value);
+    Node *previous = 0;
+    Node *i = m_table[h];
+
+    while(i && i->key != value) {
+        previous = i;
+        i = i->next;
+    }
+
+    if(!i)
+        return false;
+
+    if(previous)
+        previous->next = i->next;
+    else {
+        if(i->next)
+            m_table.insert(h, i->next);
+	else
+            m_table.remove(h);
+    }
+
+    delete i;
+
+    return true;
+}
+
+template <class T>
+void Hash<T>::deleteNode(Node *n)
+{
+    if(n) {
+        deleteNode(n->next);
+        delete n;
+    }
+}
 
 #endif
