454b94cf407961b60025662880f87a943535d87b
Scott Wheeler <wheeler@kde.org>
2004-04-12 15:33:07 +0000

    Backporting: Since there's the possiblilty that we're setting this to null, remember to check for it...
    svn path=/branches/KDE_3_2_BRANCH/kdemultimedia/juk/; revision=303078

--- playlistsplitter.cpp
+++ playlistsplitter.cpp
@@ -848,7 +848,8 @@ void PlaylistSplitter::readPlaylists()
 	    if(version == 2) {
 		Q_INT32 sortColumn;
 		s >> sortColumn;
-		playlist->setSorting(sortColumn);
+		if(playlist)
+		    playlist->setSorting(sortColumn);
 	    }
 	}
 	break;
