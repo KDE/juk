c7d5e31ad833bdfbed8420e7cbad9961bd23e50d
Michael Pyne <mpyne@kde.org>
2004-06-06 06:09:29 +0000

    Fix bug 70699, where the Need Value test for the Track wasn't correct in the file renamer dialog box.
    Thiago, thanks for the patch, but I think that was for a different bug. :-)
    
    CCMAIL:70699-done@bugs.kde.org
    
    svn path=/trunk/kdemultimedia/juk/; revision=318109

--- filerenamer.cpp
+++ filerenamer.cpp
@@ -144,6 +144,9 @@ QString FileRenamer::expandToken(TokenType type, const QString &value_) const
         value.replace(QDir::separator(), "");
     }
 
+    if((type == Track) && needValue && (value.toUInt() == 0))
+        return QString();
+
     if((needValue) && value.isEmpty())
         return QString();
 
