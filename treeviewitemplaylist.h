3d45af05ca513c96aa6c0eca97978a50cef7820c
Michael Pyne <mpyne@kde.org>
2004-06-22 18:57:59 +0000

    I forgot to add license and contact information to the two files I added for drag-and-drop retagging. CVS_SILENT
    svn path=/trunk/kdemultimedia/juk/; revision=322883

--- treeviewitemplaylist.h
+++ treeviewitemplaylist.h
@@ -1,3 +1,18 @@
+/***************************************************************************
+    begin                : Mon Jun 21 2004
+    copyright            : (C) 2004 by Michael Pyne
+    email                : pynm0001@comcast.net
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef TREEVIEWITEMPLAYLIST_H
 #define TREEVIEWITEMPLAYLIST_H
 
