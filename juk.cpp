7cf9706196f0897be0a349571ba2d8835d38b163
Michael Pyne <mpyne@kde.org>
2004-09-19 08:34:04 +0000

    Backport fix for bug 87244 to KDE 3.3.
    CCMAIL:87244@bugs.kde.org
    
    svn path=/branches/KDE_3_3_BRANCH/kdemultimedia/juk/; revision=347573

--- juk.cpp
+++ juk.cpp
@@ -73,7 +73,10 @@ JuK::JuK(QWidget *parent, const char *name) :
 
 JuK::~JuK()
 {
-
+    action("stop")->activate();
+    delete m_systemTray;
+    m_systemTray = 0;
+    delete m_splitter;
 }
 
 KActionCollection *JuK::actionCollection() const
@@ -329,13 +332,9 @@ bool JuK::queryExit()
     m_startDocked = !isVisible();
 
     hide();
-    action("stop")->activate();
-    delete m_systemTray;
-    m_systemTray = 0;
 
     Cache::instance()->save();
     saveConfig();
-    delete m_splitter;
     return true;
 }
 
