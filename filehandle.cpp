01b5ed169762f80b5e6b13888a85725d3ee9f311
Scott Wheeler <wheeler@kde.org>
2006-02-09 23:13:44 +0000

    Just an extra sanity check that I had sitting around.
    svn path=/branches/KDE/3.5/kdemultimedia/juk/; revision=507751

--- filehandle.cpp
+++ filehandle.cpp
@@ -99,6 +99,10 @@ FileHandle::FileHandle()
 FileHandle::FileHandle(const FileHandle &f) :
     d(f.d)
 {
+    if(!d) {
+	kdDebug(65432) << "The source FileHandle was not initialized." << endl;
+	d = null().d;
+    }
     d->ref();
 }
 
