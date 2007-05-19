fe492e0664adcfbc2f33a11194757022c32ec308
Michael Pyne <mpyne@kde.org>
2007-05-19 02:06:17 +0000

    Fix address to FSF to fix another Krazy issue.
    svn path=/trunk/KDE/kdemultimedia/juk/; revision=666184

--- ktrm.cpp
+++ ktrm.cpp
@@ -15,8 +15,8 @@
  *                                                                         *
  *   You should have received a copy of the GNU Lesser General Public      *
  *   License along with this library; if not, write to the Free Software   *
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
- *   USA                                                                   *
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA         *
+ *   02110-1301, USA                                                       *
  ***************************************************************************/
 
 #include "ktrm.h"
@@ -30,9 +30,10 @@
 #include <kurl.h>
 #include <kdebug.h>
 #include <kio/job.h>
-#include <qmutex.h>
+
+#include <QMutex>
 #include <QRegExp>
-#include <qevent.h>
+#include <QEvent>
 #include <QObject>
 #include <QFile>
 #include <QCustomEvent>
