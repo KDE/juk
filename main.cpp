b8d97f6ad4dc58441f9e3b100150ee753c3a4c94
Michael Pyne <mpyne@kde.org>
2008-01-09 03:54:23 +0000

    Bump trunk version before I forget
    svn path=/trunk/KDE/kdemultimedia/juk/; revision=758776

--- main.cpp
+++ main.cpp
@@ -44,8 +44,8 @@ static const char pascal[]      = I18N_NOOP("Gimper of splash screen");
 int main(int argc, char *argv[])
 {
     KAboutData aboutData("juk", 0, ki18n("JuK"),
-                         "3.0", ki18n(description), KAboutData::License_GPL,
-                         ki18n("© 2002 - 2007, Scott Wheeler"), KLocalizedString(),
+                         "3.1", ki18n(description), KAboutData::License_GPL,
+                         ki18n("© 2002 - 2008, Scott Wheeler"), KLocalizedString(),
                          "http://developer.kde.org/~wheeler/juk.html");
 
     aboutData.addAuthor(ki18n("Scott Wheeler"), ki18n(scott), "wheeler@kde.org");
