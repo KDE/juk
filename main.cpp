f8c911d7f597aa5c600fd92ab63b4bcb9939607d
Michael Pyne <mpyne@kde.org>
2009-01-11 20:57:57 +0000

    Reverting change to i18n
    svn path=/branches/KDE/4.2/kdemultimedia/juk/; revision=909560

--- main.cpp
+++ main.cpp
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 {
     KAboutData aboutData("juk", 0, ki18n("JuK"),
                          "3.2.1", ki18n(description), KAboutData::License_GPL,
-                         ki18n("© 2002 - 2009, Scott Wheeler"), KLocalizedString(),
+                         ki18n("© 2002 - 2008, Scott Wheeler"), KLocalizedString(),
                          "http://developer.kde.org/~wheeler/juk.html");
 
     aboutData.addAuthor(ki18n("Scott Wheeler"), ki18n(scott), "wheeler@kde.org");
