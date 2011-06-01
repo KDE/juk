be36fe1b9350731c49efab5cfabe09490a8fd029
Giorgos Kylafas <gekylafas@gmail.com>
2011-06-01 14:48:31 +0000

    juk: show only the selected columns in new playlists.
    Whenever a new playlist was created (e.g. "File->New->Empty Playlist", a
    DynamicPlaylist is created etc.), all the available columns were shown
    instead of only those that the user had selected.
    
    The problem was that slotInitialize() (which adds all the columns to the
    playlist) was called _after_ Playlist::SharedSettings::apply() (which
    sets the visible and hidden columns).
    
    So, make sure slotInitialize() is called during the Playlist ctor.
    
    This commit is against trunk.
    CCBUG: 243449
    
    svn path=/trunk/KDE/kdemultimedia/juk/; revision=1234560

--- playlist.cpp
+++ playlist.cpp
@@ -1695,7 +1695,10 @@ void Playlist::setup()
     m_headerMenu = m_columnVisibleAction->menu();
 
     // TODO: Determine if other stuff in setup must happen before slotInitialize().
-    QTimer::singleShot(0, this, SLOT(slotInitialize()));
+
+    // Explicitly call slotInitialize() so that the columns are added before
+    // SharedSettings::apply() sets the visible and hidden ones.
+    slotInitialize();
 }
 
 void Playlist::loadFile(const QString &fileName, const QFileInfo &fileInfo)
