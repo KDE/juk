cbe82c5c16ddafe1e3719edbe998070f495551e5
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2012-05-09 03:33:15 +0200

    clean up a bit, add copyright headers

--- lyricswidget.h
+++ lyricswidget.h
@@ -1,3 +1,18 @@
+/***************************************************************************
+    begin                : Wed May 9 2012
+    copyright            : (C) 2012 by Martin Sandsmark
+    email                : martin.sandsmark@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef LYRICSWIDGET_H
 #define LYRICSWIDGET_H
 
@@ -25,6 +40,8 @@ public Q_SLOTS:
 private Q_SLOTS:
     void receiveListReply(QNetworkReply*);
     void receiveLyricsReply(QNetworkReply*);
+    void saveConfig();
+    
     
 private:
     QNetworkAccessManager *m_networkAccessManager;
