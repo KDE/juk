a0ae371314a256f305913d02b0e2ae9763b88a55
Arnold Dumas <contact@arnolddumas.fr>
2014-03-22 14:30:22 +0100

    Port from K3Url to KUrl.

--- playlistbox.cpp
+++ playlistbox.cpp
@@ -17,7 +17,6 @@
 
 #include <kicon.h>
 #include <kiconloader.h>
-#include <k3urldrag.h>
 #include <kmessagebox.h>
 #include <kmenu.h>
 #include <kaction.h>
@@ -491,7 +490,7 @@ void PlaylistBox::contentsDragMoveEvent(QDragMoveEvent *e)
     //
     // Otherwise, do not accept the event.
 
-    if(!K3URLDrag::canDecode(e)) {
+    if (!KUrl::List::canDecode(e->mimeData())) {
         e->setAccepted(false);
         return;
     }
