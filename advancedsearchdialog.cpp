KDE/juk

commit 8dc8504185ef34e37a284241230857713600bfae

Proxify PlaylistSearch

advancedsearchdialog.cpp
@@ -133,16 +133,16 @@ AdvancedSearchDialog::AdvancedSearchDialog(const QString &defaultName,
 
 void AdvancedSearchDialog::accept()
 {
-    m_search.clearPlaylists();
-    m_search.clearComponents();
+    m_search->clearPlaylists();
+    m_search->clearComponents();
 
-    m_search.addPlaylist(CollectionList::instance());
+    m_search->addPlaylist(CollectionList::instance());
 
     for(const auto &searchLine : m_searchLines)
-        m_search.addComponent(searchLine->searchComponent());
+        m_search->addComponent(searchLine->searchComponent());
 
     PlaylistSearch::SearchMode m = PlaylistSearch::SearchMode(!m_matchAnyButton->isChecked());
-    m_search.setSearchMode(m);
+    m_search->setSearchMode(m);
 
     m_playlistName = m_playlistNameLineEdit->text();
 

advancedsearchdialog.h
@@ -38,7 +38,7 @@ public:
             const PlaylistSearch& defaultSearch = PlaylistSearch(),
             QWidget* parent = nullptr);
 
-    PlaylistSearch resultSearch() const
+    PlaylistSearch* resultSearch() const
     {
         return m_search;
     }
@@ -58,7 +58,7 @@ private:
     void updateButtons();
 
     QBoxLayout *m_criteriaLayout;
-    PlaylistSearch m_search;
+    PlaylistSearch* m_search;
     QString m_playlistName;
     QList<SearchLine *> m_searchLines;
     QLineEdit *m_playlistNameLineEdit;

playlistcollection.cpp
@@ -244,7 +244,7 @@ void PlaylistCollection::showMore(const QString &artist, const QString &album)
     PlaylistSearch search(playlists, components, PlaylistSearch::MatchAll);
 
     if(m_showMorePlaylist)
-        m_showMorePlaylist->setPlaylistSearch(search);
+        m_showMorePlaylist->setPlaylistSearch(&search);
     else
         m_showMorePlaylist = new SearchPlaylist(this, search, i18n("Now Playing"), false, true);
 
@@ -455,7 +455,7 @@ void PlaylistCollection::editSearch()
         return;
 
     auto searchDialog = new AdvancedSearchDialog(
-            p->name(), p->playlistSearch(), JuK::JuKInstance());
+            p->name(), *(p->playlistSearch()), JuK::JuKInstance());
     QObject::connect(searchDialog, &QDialog::finished, [searchDialog, p](int result)
             {
                 if (result) {
@@ -538,7 +538,7 @@ void PlaylistCollection::createSearchPlaylist()
                 if (result) {
                     raise(new SearchPlaylist(
                                 this,
-                                searchDialog->resultSearch(),
+                                *searchDialog->resultSearch(),
                                 searchDialog->resultPlaylistName()));
                 }
                 searchDialog->deleteLater();

playlistsearch.cpp
@@ -13,6 +13,15 @@
  * You should have received a copy of the GNU General Public License along with
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include <QtGlobal>
+
+#include <algorithm>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 13, 0)
+#include <QConcatenateTablesProxyModel>
+#else
+#include <KConcatenateRowsProxyModel>
+typedef KConcatenateRowsProxyModel QConcatenateTablesProxyModel;
+#endif
 
 #include "playlistsearch.h"
 #include "playlist.h"
@@ -26,7 +35,8 @@
 // public methods
 ////////////////////////////////////////////////////////////////////////////////
 
-PlaylistSearch::PlaylistSearch() :
+PlaylistSearch::PlaylistSearch(QObject* parent) :
+    QSortFilterProxyModel(parent),
     m_mode(MatchAny)
 {
 
@@ -35,81 +45,55 @@ PlaylistSearch::PlaylistSearch() :
 PlaylistSearch::PlaylistSearch(const PlaylistList &playlists,
                                const ComponentList &components,
                                SearchMode mode,
-                               bool searchNow) :
+                               QObject* parent) :
+    QSortFilterProxyModel(parent),
     m_playlists(playlists),
     m_components(components),
     m_mode(mode)
 {
-    if(searchNow)
-        search();
+    QConcatenateTablesProxyModel* const model = new QConcatenateTablesProxyModel(this);
+    for(Playlist* playlist : playlists)
+        model->addSourceModel(playlist->model());
+    setSourceModel(model);
 }
 
-void PlaylistSearch::search()
+bool PlaylistSearch::checkItem(QModelIndex *item)
 {
-    m_items.clear();
-    m_matchedItems.clear();
-    m_unmatchedItems.clear();
-
-    // This really isn't as bad as it looks.  Despite the four nexted loops
-    // most of the time this will be searching one playlist for one search
-    // component -- possibly for one column.
-
-    // Also there should be some caching of previous searches in here and
-    // allowance for appending and removing chars.  If one is added it
-    // should only search the current list.  If one is removed it should
-    // pop the previous search results off of a stack.
-
-    foreach(Playlist *playlist, m_playlists) {
-        if(!isEmpty()) {
-            for(QTreeWidgetItemIterator it(playlist); *it; ++it)
-                checkItem(static_cast<PlaylistItem *>(*it));
-        }
-        else {
-            m_items += playlist->items();
-            m_matchedItems += playlist->items();
-        }
-    }
+    return mapFromSource(static_cast<QConcatenateTablesProxyModel*>(sourceModel())->mapFromSource(*item)).isValid();
 }
 
-bool PlaylistSearch::checkItem(PlaylistItem *item)
+QModelIndexList PlaylistSearch::matchedItems() const{
+    QModelIndexList res;
+    for(int row = 0; row < rowCount(); ++row)
+        res.append(mapToSource(index(row, 0)));
+    return res;
+}
+
+void PlaylistSearch::addPlaylist(Playlist* p)
 {
-    m_items.append(item);
-
-    // set our default
-    bool match = bool(m_mode);
-
-    ComponentList::Iterator componentIt = m_components.begin();
-    for(; componentIt != m_components.end(); ++componentIt) {
-
-        bool componentMatches = (*componentIt).matches(item);
-
-        if(componentMatches && m_mode == MatchAny) {
-            match = true;
-            break;
-        }
-
-        if(!componentMatches && m_mode == MatchAll) {
-            match = false;
-            break;
-        }
-    }
-
-    if(match)
-        m_matchedItems.append(item);
-    else
-        m_unmatchedItems.append(item);
-
-    return match;
+    static_cast<QConcatenateTablesProxyModel*>(sourceModel())->addSourceModel(p->model());
+    m_playlists.append(p);
 }
+
+void PlaylistSearch::clearPlaylists()
+{
+    beginResetModel();
+    setSourceModel(new QConcatenateTablesProxyModel(this));
+    endResetModel();
+    m_playlists.clear();
+}
+
 
 void PlaylistSearch::addComponent(const Component &c)
 {
     m_components.append(c);
+    invalidateFilter();
 }
 
 void PlaylistSearch::clearComponents()
 {
     m_components.clear();
+    invalidateFilter();
 }
 
 PlaylistSearch::ComponentList PlaylistSearch::components() const
@@ -136,11 +120,13 @@ bool PlaylistSearch::isEmpty() const
     return true;
 }
 
-void PlaylistSearch::clearItem(PlaylistItem *item)
-{
-    m_items.removeAll(item);
-    m_matchedItems.removeAll(item);
-    m_unmatchedItems.removeAll(item);
+bool PlaylistSearch::filterAcceptsRow(int source_row, const QModelIndex & source_parent) const{
+    QAbstractItemModel* const model = sourceModel();
+    auto matcher = [&](Component c){
+        return c.matches(source_row, source_parent, model);
+    };
+    return m_mode == MatchAny? std::any_of(m_components.begin(), m_components.end(), matcher) :
+        std::all_of(m_components.begin(), m_components.end(), matcher);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -180,55 +166,39 @@ PlaylistSearch::Component::Component(const QRegExp &query, const ColumnList& col
 
 }
 
-bool PlaylistSearch::Component::matches(PlaylistItem *item) const
+bool PlaylistSearch::Component::matches(int row, QModelIndex parent, QAbstractItemModel* model) const
 {
-    if((m_re && m_queryRe.isEmpty()) || (!m_re && m_query.isEmpty()))
-        return false;
-
-    if(m_columns.isEmpty()) {
-        Playlist *p = static_cast<Playlist *>(item->treeWidget());
-        for(int i = 0; i < p->columnCount(); i++) {
-            if(!p->isColumnHidden(i))
-                m_columns.append(i);
-        }
-    }
-
-
-    for(ColumnList::Iterator it = m_columns.begin(); it != m_columns.end(); ++it) {
-
-        if(m_re) {
-            if(item->text(*it).contains(m_queryRe))
-                return true;
-            else
-                break;
+    for(int column : m_columns){
+        const QString str = model->index(row, column, parent).data().toString();
+        if(m_re){
+            return str.contains(m_queryRe);
         }
 
         switch(m_mode) {
         case Contains:
-            if(item->text(*it).contains(m_query, m_caseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive))
+            if(str.contains(m_query, m_caseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive))
                 return true;
             break;
         case Exact:
-            if(item->text(*it).length() == m_query.length()) {
+            if(str.length() == m_query.length()) {
                 if(m_caseSensitive) {
-                    if(item->text(*it) == m_query)
+                    if(str == m_query)
                         return true;
                 }
-                else if(item->text(*it).toLower() == m_query.toLower())
+                else if(str.toLower() == m_query.toLower())
                     return true;
             }
             break;
         case ContainsWord:
         {
-            QString s = item->text(*it);
-            int i = s.indexOf(m_query, 0, m_caseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive );
+            int i = str.indexOf(m_query, 0, m_caseSensitive ? Qt::CaseSensitive : Qt::CaseInsensitive );
 
             if(i >= 0) {
 
                 // If we found the pattern and the lengths are the same, then
                 // this is a match.
 
-                if(s.length() == m_query.length())
+                if(str.length() == m_query.length())
                     return true;
 
                 // First: If the match starts at the beginning of the text or the
@@ -241,14 +211,13 @@ bool PlaylistSearch::Component::matches(PlaylistItem *item) const
 
                 // ...then we have a match
 
-                if((i == 0 || !s.at(i - 1).isLetterOrNumber()) &&
-                   (i + m_query.length() == s.length() || !s.at(i + m_query.length()).isLetterOrNumber()))
+                if((i == 0 || !str.at(i - 1).isLetterOrNumber()) &&
+                    (i + m_query.length() == str.length() || !str.at(i + m_query.length()).isLetterOrNumber()))
                     return true;
-                break;
             }
         }
         }
-    }
+    };
     return false;
 }
 

searchplaylist.cpp
@@ -28,22 +28,22 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 SearchPlaylist::SearchPlaylist(PlaylistCollection *collection,
-                               const PlaylistSearch &search,
+                               const PlaylistSearch& search,
                                const QString &name,
                                bool setupPlaylist,
                                bool synchronizePlaying) :
     DynamicPlaylist(search.playlists(), collection, name, "edit-find",
                     setupPlaylist, synchronizePlaying),
-    m_search(search)
+    m_search(&search)
 {
 
 }
 
-void SearchPlaylist::setPlaylistSearch(const PlaylistSearch &s, bool update)
+void SearchPlaylist::setPlaylistSearch(const PlaylistSearch* s, bool update)
 {
     m_search = s;
     if(update)
-        setPlaylists(s.playlists());
+        setPlaylists(s->playlists());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -55,12 +55,14 @@ void SearchPlaylist::updateItems()
     // Here we don't simply use "clear" since that would involve a call to
     // items() which would in turn call this method...
 
-    m_search.search();
-    synchronizeItemsTo(m_search.matchedItems());
+    PlaylistItemList items;
+    for(const QModelIndex index: m_search->matchedItems())
+        items.push_back(static_cast<PlaylistItem*>(itemFromIndex(index)));
+    synchronizeItemsTo(items);
 
     if(synchronizePlaying()) {
         qCDebug(JUK_LOG) << "synchronizing playing";
-        synchronizePlayingItems(m_search.playlists(), true);
+        synchronizePlayingItems(m_search->playlists(), true);
     }
 }
 
@@ -89,7 +91,7 @@ QDataStream &operator>>(QDataStream &s, SearchPlaylist &p)
         throw BICStreamException();
 
     p.setName(name);
-    p.setPlaylistSearch(search, false);
+    p.setPlaylistSearch(&search, false);
 
     return s;
 }

searchplaylist.h
@@ -24,13 +24,13 @@ class SearchPlaylist : public DynamicPlaylist
     Q_OBJECT
 public:
     explicit SearchPlaylist(PlaylistCollection *collection,
-                   const PlaylistSearch &search = PlaylistSearch(),
+                   const PlaylistSearch& search = PlaylistSearch(),
                    const QString &name = QString(),
                    bool setupPlaylist = true,
                    bool synchronizePlaying = false);
 
-    PlaylistSearch playlistSearch() const { return m_search; }
-    void setPlaylistSearch(const PlaylistSearch &s, bool update = true);
+    const PlaylistSearch* playlistSearch() const { return m_search; }
+    void setPlaylistSearch ( const PlaylistSearch* s, bool update = true );
     virtual bool searchIsEditable() const override { return true; }
 
 protected:
@@ -40,7 +40,7 @@ protected:
     virtual void updateItems() override;
 
 private:
-    PlaylistSearch m_search;
+    const PlaylistSearch* m_search;
 };
 
 QDataStream &operator<<(QDataStream &s, const SearchPlaylist &p);

CMakeLists.txt
@@ -205,6 +205,10 @@ feature_summary(WHAT ALL INCLUDE_QUIET_PACKAGES FATAL_ON_MISSING_REQUIRED_PACKAG
 
 install(TARGETS juk  ${INSTALL_TARGETS_DEFAULT_ARGS} )
 
+if(Qt5Widgets_VERSION VERSION_LESS "5.13.0")
+    find_package(KF5 ${KF5_MIN_VERSION} REQUIRED COMPONENTS ItemModels)
+    target_link_libraries(juk KF5::ItemModels)
+endif(Qt5Widgets_VERSION VERSION_LESS "5.13.0")
 
 ########### install files ###############
 

coverinfo.cpp
@@ -197,7 +197,9 @@ void CoverInfo::applyCoverToWholeAlbum(bool overwriteExistingCovers) const
 
     // Search done, iterate through results.
 
-    PlaylistItemList results = search.matchedItems();
+    PlaylistItemList results;
+    for(QModelIndex i : search.matchedItems())
+        results.append(static_cast<PlaylistItem*>(CollectionList::instance()->itemAt(i.row(), i.column())));
     PlaylistItemList::ConstIterator it = results.constBegin();
     for(; it != results.constEnd(); ++it) {
 

commit b19e057c7bd452feed823b294cfe476fe8c76079

Fix SearchPlaylist creation
PlaylistSearch was not set. It also cannot be constant as it is
editable, leading to a cascade of changes that assumes the constantcy.

This fixes the regression caused by commit
8dc8504185ef34e37a284241230857713600bfae

advancedsearchdialog.cpp
@@ -38,9 +38,10 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 AdvancedSearchDialog::AdvancedSearchDialog(const QString &defaultName,
-                                           const PlaylistSearch &defaultSearch,
+                                           PlaylistSearch &defaultSearch,
                                            QWidget *parent) :
-    QDialog(parent)
+    QDialog(parent),
+    m_search(&defaultSearch)
 {
     setWindowTitle(i18n("Create Search Playlist"));
     setObjectName(QStringLiteral("juk_advSrchDlg"));

advancedsearchdialog.h
@@ -35,7 +35,7 @@ class AdvancedSearchDialog : public QDialog
 public:
     explicit AdvancedSearchDialog(
             const QString& defaultName,
-            const PlaylistSearch& defaultSearch = PlaylistSearch(),
+            PlaylistSearch& defaultSearch,
             QWidget* parent = nullptr);
 
     PlaylistSearch* resultSearch() const

cache.cpp
@@ -73,7 +73,7 @@ static void parsePlaylistStream(QDataStream &s, PlaylistCollection *collection)
         switch(playlistType) {
         case Search:
         {
-            SearchPlaylist *p = new SearchPlaylist(collection);
+            SearchPlaylist *p = new SearchPlaylist(collection, *(new PlaylistSearch(JuK::JuKInstance())));
             s >> *p;
             playlist = p;
             break;

playlistcollection.cpp
@@ -532,7 +532,7 @@ void PlaylistCollection::createSearchPlaylist()
     QString name = uniquePlaylistName(i18n("Search Playlist"));
 
     auto searchDialog = new AdvancedSearchDialog(
-            name, PlaylistSearch(), JuK::JuKInstance());
+            name, *(new PlaylistSearch(JuK::JuKInstance())), JuK::JuKInstance());
     QObject::connect(searchDialog, &QDialog::finished, [searchDialog, this](int result)
             {
                 if (result) {

playlistsearch.cpp
@@ -77,9 +77,7 @@ void PlaylistSearch::addPlaylist(Playlist* p)
 
 void PlaylistSearch::clearPlaylists()
 {
-    beginResetModel();
     setSourceModel(new QConcatenateTablesProxyModel(this));
-    endResetModel();
     m_playlists.clear();
 }
 

searchplaylist.cpp
@@ -28,7 +28,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 SearchPlaylist::SearchPlaylist(PlaylistCollection *collection,
-                               const PlaylistSearch& search,
+                               PlaylistSearch& search,
                                const QString &name,
                                bool setupPlaylist,
                                bool synchronizePlaying) :
@@ -39,7 +39,7 @@ SearchPlaylist::SearchPlaylist(PlaylistCollection *collection,
 
 }
 
-void SearchPlaylist::setPlaylistSearch(const PlaylistSearch* s, bool update)
+void SearchPlaylist::setPlaylistSearch(PlaylistSearch* s, bool update)
 {
     m_search = s;
     if(update)

searchplaylist.h
@@ -24,13 +24,13 @@ class SearchPlaylist : public DynamicPlaylist
     Q_OBJECT
 public:
     explicit SearchPlaylist(PlaylistCollection *collection,
-                   const PlaylistSearch& search = PlaylistSearch(),
+                   PlaylistSearch& search,
                    const QString &name = QString(),
                    bool setupPlaylist = true,
                    bool synchronizePlaying = false);
 
-    const PlaylistSearch* playlistSearch() const { return m_search; }
-    void setPlaylistSearch ( const PlaylistSearch* s, bool update = true );
+    PlaylistSearch* playlistSearch() const { return m_search; }
+    void setPlaylistSearch ( PlaylistSearch* s, bool update = true );
     virtual bool searchIsEditable() const override { return true; }
 
 protected:
@@ -40,7 +40,7 @@ protected:
     virtual void updateItems() override;
 
 private:
-    const PlaylistSearch* m_search;
+    PlaylistSearch* m_search;
 };
 
 QDataStream &operator<<(QDataStream &s, const SearchPlaylist &p);

treeviewitemplaylist.cpp
@@ -29,7 +29,7 @@
 #include "juk_debug.h"
 
 TreeViewItemPlaylist::TreeViewItemPlaylist(PlaylistCollection *collection,
-                                           const PlaylistSearch &search,
+                                           PlaylistSearch &search,
                                            const QString &name) :
     SearchPlaylist(collection, search, name, false)
 {

treeviewitemplaylist.h
@@ -28,7 +28,7 @@ class TreeViewItemPlaylist : public SearchPlaylist
 
 public:
     explicit TreeViewItemPlaylist(PlaylistCollection *collection,
-                         const PlaylistSearch &search = PlaylistSearch(),
+                         PlaylistSearch &search,
                          const QString &name = QString());
 
     virtual bool searchIsEditable() const override { return false; }